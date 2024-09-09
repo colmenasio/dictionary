3da45aaae488adc5c12a2c8e8b6dc9e52596de26
Colmenero <a.colmenero.fernandez@gmail.com>
2024-09-09 18:30:35 +0200

    Fixed T indexing and circular shift issue

--- src/md5.c
+++ src/md5.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "md5.h"
 
-#define _op(A, B, C, D, k, s, i, aux_func) _operation(&A, &B, &C, &D, k, s, i, aux_func, buffer, total_buff_len)
+#define _op(A, B, C, D, k, s, i, aux_func) _operation(&A, &B, &C, &D, k, s, i-1, aux_func, X)
 
+uint8_t get_byte(uint8_t* buffer, uint64_t total_buff_len, uint64_t byte_index);
+uint32_t get_word(uint8_t* buffer, uint64_t total_buff_len, uint64_t byte_index);
+void fill_block(uint32_t* word_buff, uint8_t* og_buffer, uint64_t og_buff_len, uint32_t block_n);
 uint32_t _F(uint32_t X, uint32_t Y, uint32_t Z);
 uint32_t _G(uint32_t X, uint32_t Y, uint32_t Z);
 uint32_t _H(uint32_t X, uint32_t Y, uint32_t Z);
 uint32_t _I(uint32_t X, uint32_t Y, uint32_t Z);
-uint8_t get_byte(uint8_t* buffer, uint64_t total_buff_len, uint64_t byte_index);
-uint32_t get_word(uint8_t* buffer, uint64_t total_buff_len, uint64_t byte_index);
 void _operation(uint32_t * A, uint32_t * B,uint32_t * C, uint32_t * D, uint32_t k, uint32_t s, uint32_t i,
-    uint32_t(*aux_func)(uint32_t, uint32_t, uint32_t) ,uint8_t* buffer, uint64_t total_buff_len);
+    uint32_t(*aux_func)(uint32_t, uint32_t, uint32_t) ,uint32_t* curr_block);
 
 
 static const uint32_t T[64] = {
@@ -26,44 +27,52 @@ static const uint32_t T[64] = {
 
 // buffer is a pointer to the start ot the message to be digested
 // byte_len is the length in bytes of said message
-struct __128bits *md5_digest(void* buffer, uint64_t total_buff_len) {
+struct hash128 md5_digest(void* buffer, uint64_t total_buff_len) {
     // INITIAL VALUES
-    uint32_t A = 0x67452301, AA = A;
-    uint32_t B = 0xEFCDAB89, BB = B;
-    uint32_t C = 0x98BADCFE, CC = C;
-    uint32_t D = 0x10325476, DD = D;
+    uint32_t A = 0x67452301, AA;
+    uint32_t B = 0xEFCDAB89, BB;
+    uint32_t C = 0x98BADCFE, CC;
+    uint32_t D = 0x10325476, DD;
+
+    uint32_t X[16];
 
     // For each 512 bit block (16 words) do the following operations
     int n_of_blocks = (total_buff_len/64)+(total_buff_len%64>=56)+1;
     for (int block_n=0;block_n<n_of_blocks;block_n++) {
-        // TODO COPY BLOCK TO A TEMPORAL BUFFER TO AVOID EXTRA CALLS TO _get_word()
+        // Initialize registers adn fill block buffer
+        AA = A; BB = B; CC = C; DD = D;
+        fill_block(X, buffer, total_buff_len, block_n);
+
         // Round 1
         _op(A, B, C, D, 0, 7, 1, _F); _op(D, A, B, C, 1, 12, 2, _F); _op(C, D, A, B, 2, 17, 3, _F); _op(B, C, D, A, 3, 22, 4, _F);
         _op(A, B, C, D, 4, 7, 5, _F); _op(D, A, B, C, 5, 12, 6, _F); _op(C, D, A, B, 6, 17, 7, _F); _op(B, C, D, A, 7, 22, 8, _F);
         _op(A, B, C, D, 8, 7, 9, _F); _op(D, A, B, C, 9, 12, 10, _F); _op(C, D, A, B, 10, 17, 11, _F); _op(B, C, D, A, 11, 22, 12, _F);
         _op(A, B, C, D, 12, 7, 13, _F); _op(D, A, B, C, 13, 12, 14, _F); _op(C, D, A, B, 14, 17, 15, _F); _op(B, C, D, A, 15, 22, 16, _F);
 
-        // Round 2 UNIMPLEMENTED LMAOOOO (0000x0x0)
-        _op(A, B, C, D, 1, 5, 17, _G); _op(D, A, B, C, 6, 9, 18, _G); _op(C, D, A, B, 11, 14, 3, _G); _op(B, C, D, A, 3, 20, 4, _G);
-        _op(A, B, C, D, 5, 5, 21, _G); _op(D, A, B, C, 10, 9, 22, _G); _op(C, D, A, B, 7, 14, 7, _G); _op(B, C, D, A, 7, 20, 8, _G);
-        _op(A, B, C, D, 9, 5, 25, _G); _op(D, A, B, C, 14, 9, 26, _G); _op(C, D, A, B, 3, 14, 11, _G); _op(B, C, D, A, 11, 20, 12, _G);
-        _op(A, B, C, D, 13, 5, 29, _G); _op(D, A, B, C, 2, 9, 30, _G); _op(C, D, A, B, 15, 14, 15, _G); _op(B, C, D, A, 15, 20, 16, _G);
+        // Round 2
+        _op(A, B, C, D, 1, 5, 17, _G); _op(D, A, B, C, 6, 9, 18, _G); _op(C, D, A, B, 11, 14, 19, _G); _op(B, C, D, A, 0, 20, 20, _G);
+        _op(A, B, C, D, 5, 5, 21, _G); _op(D, A, B, C, 10, 9, 22, _G); _op(C, D, A, B, 15, 14, 23, _G); _op(B, C, D, A, 4, 20, 24, _G);
+        _op(A, B, C, D, 9, 5, 25, _G); _op(D, A, B, C, 14, 9, 26, _G); _op(C, D, A, B, 3, 14, 27, _G); _op(B, C, D, A, 8, 20, 28, _G);
+        _op(A, B, C, D, 13, 5, 29, _G); _op(D, A, B, C, 2, 9, 30, _G); _op(C, D, A, B, 7, 14, 31, _G); _op(B, C, D, A, 12, 20, 32, _G);
 
-        // Round 3 UNIMPLEMENTED LMAOOOO (0000x0x0)
-        _op(A, B, C, D, 0, 4, 1, _H); _op(D, A, B, C, 1, 11, 2, _H); _op(C, D, A, B, 2, 16, 3, _H); _op(B, C, D, A, 3, 23, 4, _H);
-        _op(A, B, C, D, 4, 4, 5, _H); _op(D, A, B, C, 5, 11, 6, _H); _op(C, D, A, B, 6, 16, 7, _H); _op(B, C, D, A, 7, 23, 8, _H);
-        _op(A, B, C, D, 8, 4, 9, _H); _op(D, A, B, C, 9, 11, 10, _H); _op(C, D, A, B, 10, 16, 11, _H); _op(B, C, D, A, 11, 23, 12, _H);
-        _op(A, B, C, D, 12, 4, 13, _H); _op(D, A, B, C, 13, 11, 14, _H); _op(C, D, A, B, 14, 16, 15, _H); _op(B, C, D, A, 15, 23, 16, _H);
+        // Round 3
+        _op(A, B, C, D, 5, 4, 33, _H); _op(D, A, B, C, 8, 11, 34, _H); _op(C, D, A, B, 11, 16, 35, _H); _op(B, C, D, A, 14, 23, 36, _H);
+        _op(A, B, C, D, 1, 4, 37, _H); _op(D, A, B, C, 4, 11, 38, _H); _op(C, D, A, B, 7, 16, 39, _H); _op(B, C, D, A, 10, 23, 40, _H);
+        _op(A, B, C, D, 13, 4, 41, _H); _op(D, A, B, C, 0, 11, 42, _H); _op(C, D, A, B, 3, 16, 43, _H); _op(B, C, D, A, 6, 23, 44, _H);
+        _op(A, B, C, D, 9, 4, 45, _H); _op(D, A, B, C, 12, 11, 46, _H); _op(C, D, A, B, 15, 16, 47, _H); _op(B, C, D, A, 2, 23, 48, _H);
 
-        // Round 4 UNIMPLEMENTED LMAOOOO (0000x0x0)
-        _op(A, B, C, D, 0, 6, 1, _I); _op(D, A, B, C, 1, 10, 2, _I); _op(C, D, A, B, 2, 15, 3, _I); _op(B, C, D, A, 3, 21, 4, _I);
-        _op(A, B, C, D, 4, 6, 5, _I); _op(D, A, B, C, 5, 10, 6, _I); _op(C, D, A, B, 6, 15, 7, _I); _op(B, C, D, A, 7, 21, 8, _I);
-        _op(A, B, C, D, 8, 6, 9, _I); _op(D, A, B, C, 9, 10, 10, _I); _op(C, D, A, B, 10, 15, 11, _I); _op(B, C, D, A, 11, 21, 12, _I);
-        _op(A, B, C, D, 12, 6, 13, _I); _op(D, A, B, C, 13, 10, 14, _I); _op(C, D, A, B, 14, 15, 15, _I); _op(B, C, D, A, 15, 21, 16, _I);
+        // Round 4
+        _op(A, B, C, D, 0, 6, 49, _I); _op(D, A, B, C, 7, 10, 50, _I); _op(C, D, A, B, 14, 15, 51, _I); _op(B, C, D, A, 5, 21, 52, _I);
+        _op(A, B, C, D, 12, 6, 53, _I); _op(D, A, B, C, 3, 10, 54, _I); _op(C, D, A, B, 10, 15, 55, _I); _op(B, C, D, A, 1, 21, 56, _I);
+        _op(A, B, C, D, 8, 6, 57, _I); _op(D, A, B, C, 15, 10, 58, _I); _op(C, D, A, B, 6, 15, 59, _I); _op(B, C, D, A, 13, 21, 60, _I);
+        _op(A, B, C, D, 4, 6, 61, _I); _op(D, A, B, C, 11, 10, 62, _I); _op(C, D, A, B, 2, 15, 63, _I); _op(B, C, D, A, 9, 21, 64, _I);
 
+        // Update the registers
+        A += AA; B += BB; C += CC; D += DD;
     }
-    //
 
+    struct hash128 result = {.data = {A, B, C, D}};
+    return result;
 }
 
 uint8_t get_byte(uint8_t* buffer, uint64_t total_buff_len, uint64_t byte_index) {
@@ -78,12 +87,17 @@ uint32_t get_word(uint8_t* buffer, uint64_t total_buff_len, uint64_t word_index)
     uint32_t result2 = get_byte(buffer, total_buff_len, word_index*4+1);
     uint32_t result3 = get_byte(buffer, total_buff_len, word_index*4+2);
     uint32_t result4 = get_byte(buffer, total_buff_len, word_index*4+3);
-    return (result1 << 24) | (result2 << 16) | (result3 << 8) | result4;
+    return result1 | (result2 << 8) | (result3 << 16) | result4 << 24;
     // return
-    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index) << 24 |
-    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+1) << 16 |
-    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+2) << 8 |
-    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+3);
+    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index) |
+    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+1) << 8 |
+    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+2) << 16 |
+    //     (uint32_t)get_byte(buffer, total_buff_len, 4*word_index+3) << 24;
+}
+
+
+void fill_block(uint32_t* word_buff, uint8_t* og_buffer, uint64_t og_buff_len, uint32_t block_n) {
+    for (int i = 0; i < 16; i++) {word_buff[i] = get_word(og_buffer, og_buff_len, 16*block_n+i);};
 }
 
 uint32_t _F(uint32_t X, uint32_t Y, uint32_t Z) {
@@ -102,6 +116,8 @@ uint32_t _I(uint32_t X, uint32_t Y, uint32_t Z) {
     return Y ^ (X | ~Z);
 }
 
-void _operation(uint32_t * A, uint32_t * B,uint32_t * C, uint32_t * D, uint32_t k, uint32_t s, uint32_t i, uint32_t(*aux_func)(uint32_t, uint32_t, uint32_t) ,uint8_t* buffer, uint64_t total_buff_len) {
-    *A = *B + ((*A + aux_func(*B, *C, *D) + get_word(buffer, total_buff_len, k) + T[i]) << s);
+void _operation(uint32_t * A, uint32_t * B,uint32_t * C, uint32_t * D, uint32_t k, uint32_t s, uint32_t i,
+    uint32_t(*aux_func)(uint32_t, uint32_t, uint32_t) ,uint32_t* curr_block) {
+    uint32_t binding = *A + aux_func(*B, *C, *D) + curr_block[k] + T[i];
+    *A = *B + ((binding << s) | (binding >> (32-s)));
 }
