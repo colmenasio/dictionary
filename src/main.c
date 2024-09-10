0f86b6188726543e3ea4aeeabee41f61545f890d
Colmenero <a.colmenero.fernandez@gmail.com>
2024-09-10 20:12:20 +0200

    Added tests

--- src/main.c
+++ src/main.c
@@ -19,9 +19,9 @@ int main() {
     struct BUCKET b4 = make_new_bucket(k4, &d);
     struct BUCKET b5 = make_new_bucket(k5, &e);
     struct BUCKET b6 = make_new_bucket(k6, &f);
+    struct BUCKET over1 = make_new_bucket(k1, &f);
 
-    // MANUAL INSERTION
-    {
+    { // MANUAL INSERTION
         struct DICTIONARY dict = make_new_dict();
         insert_bucket(&dict, &b1);
         insert_bucket(&dict, &b2);
@@ -29,13 +29,22 @@ int main() {
         insert_bucket(&dict, &b4);
         insert_bucket(&dict, &b5);
         insert_bucket(&dict, &b6);
+        printf("Testing keys and getting\n");
         print_bucket(get_bucket(&dict, "dos"), "%d");
         printf(is_key(&dict,"cuatro")? "cuatro is key\n" : "cuatro is not key\n");
         printf(is_key(&dict,"diez")? "diez is key\n" : "diez is not key\n");
-        delete_value(&dict, "cuatro");
+        delete_value(&dict, "cuatro"); printf("deleting \"cuatro\"\n");
         printf(is_key(&dict,"cuatro")? "cuatro is key\n" : "cuatro is not key\n");
-        struct BUCKET *lmao = get_bucket(&dict, "cuatro");
-
+        print_bucket(get_bucket(&dict, "cuatro"), "%d");
+    }
+    { // AUTO INSERTION
+        int length = 6;
+        struct BUCKET *initial_buckets[] = {&b1, &b2, &b3, &b4, &b5, &b6};
+        struct  DICTIONARY dict = make_new_populated_dict(initial_buckets, length);
+        printf("\n\nTest overwriting\n");
+        print_bucket(get_bucket(&dict, "uno"), "%d");
+        insert_bucket(&dict, &over1);
+        print_bucket(get_bucket(&dict, "uno"), "%d");
     }
     return 0;
 }
